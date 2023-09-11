d4eae12cc2841e30f1bc18b1df6541c859aee615
Shunfa Xu <gouwa5700@gmail.com>
2023-09-11 22:10:35 +0800

    open all links within WebView

--- app/src/main/java/com/example/goplan/MainActivity.kt
+++ app/src/main/java/com/example/goplan/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.goplan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myWebView: WebView = findViewById(R.id.webView)
+        myWebView.webViewClient = WebViewClient()
         myWebView.settings.javaScriptEnabled = true
         myWebView.loadUrl("https://goooooouwa.fun:8095/")
     }
