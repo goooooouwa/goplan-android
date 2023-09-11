33efe963ac4d75fc6c2709fb170354013014e684
Shunfa Xu <gouwa5700@gmail.com>
2023-09-11 23:49:15 +0800

    fix: GoPlan Web client not showing up
     due to localStorage not enabled

--- app/src/main/java/com/example/goplan/MainActivity.kt
+++ app/src/main/java/com/example/goplan/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.goplan
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myWebView: WebView = findViewById(R.id.webView)
         myWebView.webViewClient = WebViewClient()
         myWebView.settings.javaScriptEnabled = true
-        myWebView.loadUrl("https://goooooouwa.fun:8096/users/sign_in")
+        myWebView.settings.setDomStorageEnabled(true);
+        myWebView.loadUrl("https://goooooouwa.fun:8095")
     }
 }
