0799f381095fb25f28a670dbf3d85e0264df75b6
Shunfa Xu <gouwa5700@gmail.com>
2023-09-12 14:43:19 +0800

    copy example WebView code
    Ref: https://www.digitalocean.com/community/tutorials/android-webview-example-tutorial

---
+++ app/src/main/java/com/example/goplan/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.goplan;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    private WebView webView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("https://goooooouwa.fun:8095");
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
